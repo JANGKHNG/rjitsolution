0a322f4c1f61db85ffc47c6387f1dc7d8e00107f
JANGKHNG <169040829+JANGKHNG@users.noreply.github.com>
2024-06-26 12:02:28 +0530

    index

---
+++ index.php
@@ -0,0 +1,199 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <title>RJ IT SOLUTION</title>
+  </head>
+  <body>
+   <header>
+  <nav class="navbar navbar-light bg-light paddingcustom">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img src="images/logo.png" alt="" class="d-inline-block align-text-top">
+        </a>
+         <ul class="nav justify-content-end">
+          <li class="nav-item linkboxactivecustom">
+            <a class="nav-link linkcustom active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item linkboxactivecustom">
+            <a class="nav-link linkcustom" href="#">Service</a>
+          </li>
+          <li class="nav-item linkboxactivecustom">
+            <a class="nav-link linkcustom" href="#">Product</a>
+          </li>
+          <li class="nav-item linkboxactivecustom">
+            <a class="nav-link linkcustom" href="#">About Us</a>
+          </li>
+          <li class="nav-item linkboxactivecustom">
+            <a class="nav-link linkcustom" href="#">Complain</a>
+          </li>
+        </ul>
+      </div>
+         
+</nav>
+</header>
+    <section class="m-1 p-1" style="background:    #f9e79f   ;">
+           <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+              <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+              </div>
+              <div class="carousel-inner">
+                <div class="carousel-item active" data-bs-ride="carousel" data-bs-interval="1090">
+                  <img src="images/1.jpg" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item" data-bs-interval="1090">
+                  <img src="images/2.jpg" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item" data-bs-interval="1090">
+                  <img src="images/3.jpg" class="d-block w-100" alt="...">
+                </div>
+              </div>
+              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+              <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+              </button>
+            </div>
+    </section>
+    <section class="m-3">
+            <div class="row g-0 position-relative" style="background:  #bdf5f1 ;">
+               <div class="col-md-6 p-4 ps-md-0">
+                <h5 class="mt-0 text-danger text-center">About Us</h5>
+                <h2 class="mt-0 text-danger text-center">Welcome to RJ IT SOLUTION, DAHEJ</h2>
+                <p class="text-center">RJ IT Solutions is an IT services and consulting company that specializes in providing end-to-end data warehouse solutions, information assurance, identity management solutions, and comprehensive cyber security offerings.</p>
+                <!-- <a href="#" class="stretched-link">Go somewhere</a> -->
+              </div>
+              <div class="col-md-6 mb-md-0 p-md-4">
+                <img src="images/4.png" class="w-100" alt="">
+              </div>
+             
+            </div>
+    </section>
+    <section class="m-3 justify-content-center p-3"  style="background:   #cbd5d4  ;">
+      <div class="row col-12">
+          <h5 class="mt-0 text-danger text-center">Service</h5>
+          <h2 class="mt-0 text-danger text-center">Our Provide Excellent Services</h2>             
+          <p class="mx-0 text-center col-12 px-5">RJ IT Solutions is an IT services and consulting company that specializes in providing end-to-end data warehouse solutions, information assurance, identity management solutions, and comprehensive cyber security offerings.</p>
+      </div>
+      <div class="row justify-content-center">
+      <div class="card-group col-8 align-item-center">
+        <div class="card mx-2" style="width: 18rem;">
+          <img src="https://www.shutterstock.com/image-vector/computer-repair-vector-260nw-17244604.jpg" class="card-img-top " height="176" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Windows Service</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+        </div>
+        <div class="card mx-2">
+          <img src="https://i.pinimg.com/originals/18/b1/91/18b19126c176a3e8a9bce2ecb75348d9.jpg" height="176" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Printer Service</h5>
+            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+          </div>
+        </div>
+        <div class="card mx-2">
+          <img src="https://www.shutterstock.com/image-vector/man-maintaining-server-computer-security-600nw-2104780022.jpg" height="176" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Software Activator</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+          </div>
+        </div>
+      </div>
+      <div class="card-group col-8 align-item-center mt-2">
+        <div class="card mx-2" style="width: 18rem;">
+          <img src="https://static.vecteezy.com/system/resources/previews/013/699/985/original/happy-man-superhero-plumber-with-wrench-ready-to-help-client-or-customer-smiling-mechanic-or-engineer-plumb-fix-repair-equipment-job-work-occupation-concept-illustration-vector.jpg" class="card-img-top " height="176" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Fast Workers</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+        </div>
+        <div class="card mx-2">
+          <img src="https://en.pimg.jp/044/988/885/1/44988885.jpg" height="176" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Any Network Related Service</h5>
+            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+          </div>
+        </div>
+        <div class="card mx-2">
+          <img src="https://img.freepik.com/premium-vector/computerlaptop-repair-service-concept-huge-laptop-happy-technician-man-flat-vector-cartoon-character-illustration_77116-2356.jpg" height="176" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">PC/Laptop Reparing</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+          </div>
+        </div>
+      </div>
+      </div>
+    </section>
+    <footer class="bg-dark text-white m-0">
+     <div class="container-fluid text-center justify-content-center">
+        <div class="row align-items-center">
+          <div class="col m-3">
+            <a href="#" class="text-white mx-1 text-decoration-none">
+             <i class="bi bi-facebook"></i>
+            </a>
+             <a href="#" class="text-white mx-1 text-decoration-none">
+             <i class="bi bi-whatsapp"></i>
+            </a>
+            <a href="#" class="text-white mx-1 text-decoration-none">
+             <i class="bi bi-instagram"></i>
+            </a>
+            <a href="#" class="text-white mx-1 text-decoration-none">
+             <i class="bi bi-linkedin"></i>
+            </a>
+            <a href="#" class="text-white mx-1 text-decoration-none">
+             <i class="bi bi-twitter"></i>
+            </a>
+          </div>
+        </div>
+     </div>
+     <div class="container-fluid text-center justify-content-center">
+        <div class="row align-items-center">
+          <div class="col m-3">
+            <a href="#" class="text-white mx-1 text-decoration-none ">Home
+            </a>
+             <a href="#" class="text-white mx-1 text-decoration-none ">Service
+            </a>
+            <a href="#" class="text-white mx-1 text-decoration-none ">Product
+            </a>
+            <a href="#" class="text-white mx-1 text-decoration-none ">About Us
+            </a>
+            <a href="#" class="text-white mx-1 text-decoration-none ">Complain
+            </a>
+          </div>
+        </div>
+     </div>
+      <div class="text-center p-1 bg-danger">
+        Copyright © 
+        <a class="text-white text-decoration-none" href="#"
+           >RJ IT SOLUTION</a
+          >
+      </div>
+    </footer>
+          <!-- End of .container -->
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> -->
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    
+    <!-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script> -->
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script> -->
+
+    <script src="js/bootstrap.js"></script>
+    
+  </body>
+</html>
